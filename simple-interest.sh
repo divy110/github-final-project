21ed9c7f579f72bbee20f75517569e703d4cf953
divy110 <divy1200@gmail.com>
2025-10-22 09:16:59 +0530

    Add simple interest calculation script
    This script calculates simple interest based on user input for principal, rate, and time.

---
+++ simple-interest.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash # This script calculates simple interest given principal, # annual rate of interest and time
+ period in years. # Author: Upkar Lidder (IBM) # Additional Authors:
+ echo "Enter the principal:" read p echo "Enter rate of interest per year:" read r echo "Enter time
+ period in years:" read t
+ s=`expr $p \* $t \* $r / 100` echo "The simple interest is:" echo $s
